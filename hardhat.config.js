6d18a225b4f7ca63ed32477756111a8626f8b07a
dvaleri <djvaleri4@gmail.com>
2021-07-21 13:51:46 -0400

    Feat: Added deployment script and set up for rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
 };
--- test/Token.js
+++ test/Token.js
@@ -1,6 +1,5 @@
 const {expect} = require("chai");
 const { ethers } = require("hardhat");
-const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
 
 describe("Token contract", function() {
     let Token;
