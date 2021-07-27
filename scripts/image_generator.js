0f0714573b1d5e00ab19dde22cbf8a98ea64ec7d
dvaleri <djvaleri4@gmail.com>
2021-07-27 17:03:59 -0400

    Created script to generate token images with dynamic text

---
+++ scripts/image_generator.js
@@ -0,0 +1,43 @@
+const { createCanvas, loadImage } = require('canvas');
+const fs = require('fs');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function getText(){
+    return new Promise((resolve) => {
+        rl.question("Enter some text: ", (input) => resolve(input));
+    });
+}
+
+async function main(){
+    const text = await getText();
+    const width = 1080;
+    const height = 1080;
+
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext('2d');
+
+    const image = await loadImage("./images/base-token-v3.png");
+    context.drawImage(image, 0, 0);
+
+    context.font = 'bold 60pt Ariel';
+    context.textBaseline = 'center';
+    context.textAlign = 'center';
+
+    context.fillStyle = '#000';
+    context.fillText(text, 540, 540);
+
+    const buffer = canvas.toBuffer('image/png');
+    fs.writeFileSync('./images/image.png', buffer);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
